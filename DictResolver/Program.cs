219b5ba5abef06cbd22a94b76636dae26006830f
Denys Latushkin <dlatushkin@deloitte.com>
2025-01-17 09:17:06 +0200

    json: use System.Text

--- DictResolver/Program.cs
+++ DictResolver/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
 
 namespace DictResolver
 {
@@ -8,6 +12,22 @@ namespace DictResolver
         static void Main(string[] args)
         {
             var content = File.ReadAllText("wavemaker.json");
+
+            //ProcessWithNewton(content);
+            ProcessWithText(content);
+
+            Console.WriteLine("done");
+        }
+
+        static void ProcessWithText(string content)
+        {
+            JsonNode forecastNode = JsonNode.Parse(content);
+            var leaves = new List<Leaf>();
+            VisitNode(forecastNode, leaves);
+        }
+
+        static void ProcessWithNewton(string content)
+        {
             dynamic obj = JsonConvert.DeserializeObject(content);
 
             var leaves = new List<Leaf>();
@@ -31,8 +51,37 @@ namespace DictResolver
                 leaf.Val = resolvedLeaf.Val;
                 Console.WriteLine("{0} substituted by {1}", s, resolvedLeaf.Val);
             }
+        }
 
-            Console.WriteLine("Hello, World!");
+        static void VisitNode(JsonNode parentNode, List<Leaf> leaves)
+        {
+            if (parentNode is JsonObject parentObj)
+            {
+                foreach (var childObj in parentObj)
+                {
+                    VisitNode(childObj.Value, leaves);
+                }
+            }
+            //else if (parentNode.GetType() == typeof(JsonElement))
+            //{
+            //    var element = (JsonElement)parentNode;
+            //    foreach (var childObj in parentObj)
+            //    {
+            //        VisitNode(childObj.Value, leaves);
+            //    }
+            //}
+        }
+
+        //static void VisitNode(JsonValuePrimitive element, List<Leaf> leaves)
+        //{
+
+        //}
+
+        static void VisitNode(JsonObject parentNode, List<Leaf> leaves)
+        {
+            //foreach (var childNode in parentNode.)
+            //{
+            //}
         }
 
         internal class Leaf
